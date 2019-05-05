cb9e6e51e1a524e23b577738f2e49f8965770ba8
Hachibkhan <hachib.khan19@gmail.com>
2019-05-05 08:08:54 +0600

    Turtle Example

---
+++ fnc.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.shape("turtle")
+
+turtle.color("green")
+
+for i in range(20):
+
+    turtle.forward(10)
+
+    turtle.penup()
+
+    turtle.forward(3)
+
+    turtle.pendown()
+
+turtle.exitonclick()
